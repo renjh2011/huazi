ada5a197c6bc7b17ac14c619c26e8c32df08efa4
root <root>
2017-04-11 17:37:42 +0800

    swagger config

---
+++ xpand-dispatcher/src/main/java/com/xpand/dispatcher/core/config/SwaggerConfig.java
@@ -0,0 +1,37 @@
+package com.xpand.dispatcher.core.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableWebMvc
+@EnableSwagger2
+@ComponentScan(basePackages = "com.xpand.dispatcher.modules")
+public class SwaggerConfig  extends WebMvcConfigurationSupport {
+	@Bean
+	public Docket api(){
+		return new Docket(DocumentationType.SWAGGER_2)
+				.apiInfo(apiInfo())
+				.select()//选择哪些路径和API会生成document
+				.apis(RequestHandlerSelectors.basePackage("com.xpand.dispatcher.modules"))   //对所有api进行监控
+                .paths(PathSelectors.any())             //对所有路径进行监控
+				.build();
+	}
+
+	private ApiInfo apiInfo(){
+		return new ApiInfoBuilder()
+				.title("xpand")
+				.version("1.0")
+				.build();
+	}
+}
